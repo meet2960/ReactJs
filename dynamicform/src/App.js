5d90b9b02cd34119e714cf04c8e9260d33e049a7
Meet Ghelani <meetghelani43@gmail.com>
2023-04-25 09:47:16 +0530

    Layout changes

--- dynamicform/src/App.js
+++ dynamicform/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "./pages/Layout";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 };
 
 export default App;
