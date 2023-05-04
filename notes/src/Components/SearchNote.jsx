320d683b326850bae93ee0cdf82b6cb4c3cf20e1
Meet Ghelani <meetghelani43@gmail.com>
2023-05-04 14:42:40 +0530

    added search file

---
+++ notes/src/Components/SearchNote.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchNote = () => {
+    return (
+        <div>
+            Search Notes Will Appear here
+        </div>
+    );
+};
+
+export default SearchNote;
