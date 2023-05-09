31c98c039afdbd0b3d8c0ccbf36472da64b1d213
Meet Ghelani <meetghelani43@gmail.com>
2023-05-09 11:36:01 +0530

    Changed layout and refactor the codes

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import Routing from "./Pages/Routing";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./assets/styles/custom.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import "./assets/styles/dark-layout.scss"
-import Routing from "./Pages/Routing";
+import "./assets/styles/app.scss"
 
 const App = () => {
   return (
