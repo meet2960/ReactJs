89e9c7821b8325a5d5e6d717021aad790c4b87d9
Meet Ghelani <meetghelani43@gmail.com>
2023-05-10 14:52:42 +0530

    changes css and added new file

---
+++ notes/src/Redux/layout.js
@@ -0,0 +1,19 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+const initialState = {
+    layoutModeType: "light",
+};
+
+export const LayoutSlices = createSlice({
+    name: 'Layout',
+    initialState: initialState,
+    reducers: {
+        changeLayoutMode: (state, action) => {
+            state.layoutModeType = action.payload
+        }
+    },
+})
+
+export const {changeLayoutMode} = LayoutSlices.actions
+export default LayoutSlices.reducer;
